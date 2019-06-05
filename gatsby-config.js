6f829d3e741b02da68ad8a5fd2efa242e8cf6d2b
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-06-05 14:15:37 -0300

    Bump

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,17 @@ module.exports = {
   plugins: [
     `gatsby-plugin-netlify`,
     `gatsby-plugin-material-ui`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Kioskify`,
+        short_name: `Kioskify`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
   ],
 }
