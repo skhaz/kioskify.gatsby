2cfb1de999c204ab820fc8152c6623c996b4338f
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-06-05 14:33:20 -0300

    Bump

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,25 @@
-const path = require(`path`)
-
 module.exports = {
   plugins: [
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify`,
     `gatsby-plugin-material-ui`,
     `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Kioskify`,
-        short_name: `Kioskify`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
+        name: "Kioskify",
+        short_name: "Kioskify",
+        start_url: "/",
+        background_color: "#673ab7",
+        theme_color: "#673ab7",
+        display: "standalone",
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: path.join(__dirname, `src`, `images`),
+        name: "images",
+        path: `${__dirname}/src/images/`,
       },
     },
     `gatsby-plugin-sharp`,
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -2,9 +2,5 @@ import React from "react"
 import Button from "@material-ui/core/Button"
 
 export default () => {
-  return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  )
+  return <Button variant="contained" color="primary" />
 }
