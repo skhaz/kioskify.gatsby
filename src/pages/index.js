b866d3557594b3febe30bdcc2d98457a15e3dc2c
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-06-05 15:12:26 -0300

    Bump

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,66 @@
 import React from "react"
-import Button from "@material-ui/core/Button"
+import Typography from "@material-ui/core/Typography"
+import { makeStyles } from "@material-ui/styles"
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    maxWidth: 500,
+  },
+})
 
 export default () => {
+  const classes = useStyles()
+
   return (
-    <Button variant="contained" color="primary">
-      1
-    </Button>
+    <div className={classes.root}>
+      <Typography component="h2" variant="h1" gutterBottom>
+        h1. Heading
+      </Typography>
+      <Typography variant="h2" gutterBottom>
+        h2. Heading
+      </Typography>
+      <Typography variant="h3" gutterBottom>
+        h3. Heading
+      </Typography>
+      <Typography variant="h4" gutterBottom>
+        h4. Heading
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        h5. Heading
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        h6. Heading
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom>
+        subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
+      <Typography variant="subtitle2" gutterBottom>
+        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore
+        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+        fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore
+        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+        fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+      <Typography variant="button" gutterBottom>
+        button text
+      </Typography>
+      <Typography variant="caption" gutterBottom>
+        caption text
+      </Typography>
+      <Typography variant="overline" gutterBottom>
+        overline text
+      </Typography>
+    </div>
   )
 }
