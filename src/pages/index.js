afa3cc5740e7d56a8fc0ae929d89c03871d54ffb
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-06-05 17:26:59 -0300

    Bump

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { Link } from "gatsby"
 export default () => {
   return (
     <>
-      <img src="https://secure.meetupstatic.com/photos/event/6/6/a/3/600_467306275.jpeg" />{" "}
     </>
   )
 }
