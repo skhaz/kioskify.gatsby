a19480055554ba8a113a27ded35a01792e8f48b7
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-06-05 15:47:33 -0300

    Bump

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,6 +1,45 @@
 import React from "react"
+
+import { ThemeProvider } from "@material-ui/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { createMuiTheme } from "@material-ui/core/styles"
+import { deepPurple, deepOrange } from "@material-ui/core/colors"
+import { makeStyles } from "@material-ui/styles"
 import Button from "@material-ui/core/Button"
 
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+  },
+})
+
+function App() {
+  const classes = useStyles()
+
+  return <Button className={classes.root} />
+}
+
 export default () => {
-  return <Button variant="contained" color="primary" />
+  const theme = createMuiTheme({
+    palette: {
+      primary: deepPurple,
+      secondary: deepOrange,
+    },
+    typography: {
+      useNextVariants: true,
+    },
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  )
 }
