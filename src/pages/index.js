b51de58b596682e437f8ef33d5a70f6eb1576fec
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-06-24 16:40:45 -0300

    Add page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,13 @@
 import React from "react"
 import App from "~/components/App"
 
-export default () => <App />
+export default () => (
+  <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Kioskify</title>
+      <link rel="canonical" href="https://kioskify.app/" />
+    </Helmet>
+    <App />
+  </>
+)
